434553dc1a750b69bc74880c455dd87fb66403bd
yazin <45133548+yayajallow@users.noreply.github.com>
2020-12-29 05:28:46 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
  //values
   $dif='';
-  $pList=array();//page list
-   $pList[]=array( 'file'=> $dif.'_pgs/doctype.php', 'name'=>'doctype' );//doctype
-   $pList[]=array( 'file'=> $dif.'_pgs/head.php', 'name'=>'head' );//head
-   $pList[]=array( 'file'=> $dif.'_pgs/bod.php', 'name'=>'bod' );//body
-   $pList[]=array( 'file'=> $dif.'_pgs/footer.php', 'name'=>'footer' );//footer
+  $pList=array(//page list
+   'doctpe'=> $dif.'_pgs/doctype.php',
+   'head'=> $dif.'_pgs/head.php',
+   'bod'=> $dif.'_pgs/bod.php',
+   'footer'=> $dif.'_pgs/footer.php'
+  );
  //includes
   include("pages.php");//includes the pages class_alias
  //classes
   $pge=new pg(array());//pages
- $pge->load($pList);//loads pages
+ $pge->load(array( 'pages'=>$pList ));//loads pages
    $pge->make(array( 'list'=>'doctype, head, bod, footer', 'return'=>'string' ));//creates the current page
   $curPage=$pge->doc(array( 'obj'=>'current' ));//gets current page
  echo $curPage;//outputs current page
